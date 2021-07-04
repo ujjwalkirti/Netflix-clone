f7ef0e87078734d28968b054279fe83412bbcc65
ujjwalkirti <kirti.chargers@gmail.com>
2021-07-04 17:18:44 +0530

    4

--- src/App.js
+++ src/App.js
@@ -3,20 +3,28 @@ import { Counter } from "./features/counter/Counter";
 import "./App.css";
 import Nav from "./components/Nav";
 import Banner from "./components/Banner";
+import Row from "./components/Row";
+import requests from "./components/Requests";
 require("dotenv").config();
 
 function App() {
-	return (
-		<div className="App">
-			<Nav />
-      		<Banner/>
-			{/* banner */}
-			{/* row */}
-			{/* row */}
-			{/* row */}
-			{/* row */}
-		</div>
-	);
+  return (
+    <div className="App">
+      <Nav />
+      <Banner />
+      <Row title="Trending" URLparams={requests.fetchTrending} true />
+      <Row title="Top Rated" URLparams={requests.fetchTopRated} false />
+      <Row title="Action" URLparams={requests.fetchActionMovies} false />
+      <Row title="Comedy" URLparams={requests.fetchComedyMovies} false />
+      <Row title="Horror" URLparams={requests.fetchHorrorMovies} false />
+      <Row title="Romance" URLparams={requests.fetchRomanceMovies} false />
+      <Row
+        title="Documentaries"
+        URLparams={requests.fetchDocumentaries}
+        false
+      />
+    </div>
+  );
 }
 
 export default App;
--- src/components/Row.js
+++ src/components/Row.js
@@ -1,11 +1,40 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
-function Row() {
-	return (
-		<div>
-			
-		</div>
-	);
+function Row({ title, URLparams, isLargeRow }) {
+  const [movies, setMovies] = useState([]);
+
+  const URLbase = "https://api.themoviedb.org/3";
+  const URLbaseImage = "https://image.tmdb.org/t/p/original/";
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(`${URLbase}${URLparams}`);
+      setMovies(request.data.results);
+      console.log(movies);
+      return request;
+    }
+    fetchData();
+  }, [URLparams]);
+  return (
+    <div className="row d-flex ml-5">
+      <h2>{title}</h2>
+      {movies.map((movie) => {
+        // const imageURL = `${URLbaseImage}${
+        //   isLargeRow ? movie?.poster_path : movie?.backdrop_path
+        // }`;
+        
+          <img
+            src={`${URLbaseImage}${
+              isLargeRow ? movie?.poster_path : movie?.backdrop_path
+            }`}
+            className="card-img-top"
+            alt={movie?.original_title}
+          />
+          
+        
+      })}
+    </div>
+  );
 }
 
 export default Row;
