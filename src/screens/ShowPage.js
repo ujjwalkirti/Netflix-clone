79215fb4f185f090111c25e43ae40762f08c90f1
ujjwal kirti <kirti.chargers@gmail.com>
2021-07-17 17:19:23 +0530

    17-07-2021

--- src/screens/ShowPage.js
+++ src/screens/ShowPage.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { useHistory } from "react-router-dom";
+import searchYoutube from "youtube-api-v3-search";
 import Nav from "../components/Nav";
 import { selectUser } from "../features/userSlice";
 import db from "../Firebase";
@@ -9,15 +10,47 @@ import "./ShowPage.css";
 function ShowPage() {
   const user = useSelector(selectUser);
   const [movie, setMovie] = useState([]);
+  const [arr_search, setArr_Search] = useState({});
+  const [id, setId] = useState("");
   const history = useHistory();
-  useEffect(() => {
-    db.collection("users")
-      .doc(user?.uid)
-      .get()
-      .then((doc) => {
+
+  db.collection("users")
+    .doc(user?.uid)
+    .onSnapshot((doc) => {
+      if (doc.data()?.wantToWatch !== null && movie === []) {
         setMovie(doc.data()?.wantToWatch);
-      });
-  });
+      }
+    });
+
+  useEffect(() => {
+    // db.collection("users")
+    //   .doc(user?.uid)
+    //   .get()
+    //   .then((doc) => {
+    //     if (doc.data()?.wantToWatch !== null && movie === []) {
+    //       setMovie(doc.data()?.wantToWatch);
+    //     } else console.log("Movie is already set!");
+    //   });
+
+    setArr_Search({
+      part: "snippet",
+      type: "video",
+      
+      maxResults: 3,
+      q: movie?.title,
+    });
+    function videoSearch(api_key, options) {
+      searchYoutube(api_key, options)
+        .then((results) => {
+          console.log(results);
+        })
+        .catch((err) => {
+          console.log(err.error.message);
+        });
+    }
+    videoSearch(process.env.REACT_APP_YOUTUBE_API_KEY, arr_search);
+  }, [movie]);
+
   function goBack(event) {
     event.preventDefault();
     db.collection("users")
